8ee1e43a48083165082afac8ff9a92bca4f7bfdd
abunuwas <joseharoperalta@gmail.com>
2016-07-26 17:32:52 +0100

    Improve ability to fetch data about sent packets

--- src/mod_hello_world.erl
+++ src/mod_hello_world.erl
@@ -16,6 +16,7 @@
 -export([
 	start/2, 
 	stop/1, 
+	packeto/4,
 	packeto/5,
 	process/2
 	]).
@@ -39,8 +40,28 @@ stop(_Host) ->
     ejabberd_hooks:delete(user_receive_packet, _Host, ?MODULE, packeto, 50),
     ok. 
 
-packeto(Packet, _state, _jid_from, _jid_to, _jid_from2) ->
-    ok = ?INFO_MSG("BUENO BUENO BUENO: a packet was sent...", []),
+packeto(Packet, _state, Jid_from, Jid_to) ->
+	{_, Username, _, _, _, _, _} = Jid_from,
+	{_, Subdomain, _, _, _, _, _} = Jid_to,
+	{_, Stanza, _, _} = Packet,
+    ok = ?INFO_MSG("BUENO BUENO BUENO: a packet was sent by ~p~n, to ~p~n, type: ~p~n", [
+    																					binary_to_list(Username),
+    																					binary_to_list(Subdomain),
+    																					binary_to_list(Stanza)
+    																					]
+    																					),
+    Packet.
+
+packeto(Packet, _state, Jid_from, Jid_to, _jid_from2) ->
+	{_, Username, _, _, _, _, _} = Jid_from,
+	{_, _, Subdomain, _, _, _, _} = Jid_to,
+	{_, Stanza, _, _} = Packet,
+    ok = ?INFO_MSG("BUENO BUENO BUENO: a packet was sent by ~p, to ~p, type: ~p~n", [
+    																					binary_to_list(Username),
+    																					binary_to_list(Subdomain),
+    																					binary_to_list(Stanza)
+    																					]
+    																					),
     Packet.
 
 process(_Path, _Request) ->
